4c5b7e5795e8cfb69b9f0ff66c7654df9ab8dbba
Doniyorbek <d.anorboyev@newuu.uz>
2023-11-14 22:13:58 +0500

    Done

--- problems/problem_5.cpp
+++ problems/problem_5.cpp
@@ -2,5 +2,19 @@ float problemSolution5(float x, float y, char operation) {
    float result;
    // write your code here
 
+    switch (operation) {
+        case '+':
+            result = x+y;
+            break;
+        case '-':
+            result = x-y;
+            break;
+        case '*':
+            result = x*y;
+            break;
+        default:
+            result = x/y;
+
+    }
    return result;
 }
